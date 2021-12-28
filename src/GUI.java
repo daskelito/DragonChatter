78bda5cbe18ea99b47478cf624ba350b46a0b403
daskelito <henrik.heinze@hotmail.com>
2021-12-28 10:41:40 +0100

    [Dragon tounge]: *Hidden message*

--- src/GUI.java
+++ src/GUI.java
@@ -6,9 +6,6 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class GUI {
-
-    private ArrayList<JCheckBox> onlineList = new ArrayList<>();
-    private ArrayList<JCheckBox> contactsList = new ArrayList<>();
     private JFrame frame;
     private JPanel rightPanel;
     private JPanel onlineListPanel;
@@ -55,52 +52,53 @@ public class GUI {
         JLabel contactTitle = new JLabel("Contacts:");
         contactsListPanel.add(contactTitle);
         rightPanel.add(contactsListPanel);
-        System.out.println(contactTitle.getName());
 
         frame.setVisible(true);
 
-        addOnline("nigger");
-        paintLists();
-        removeOnline("nigger");
-        paintLists();
+        addOnline("theo");
+        addOnline("hora");
+
+        System.out.println(getListofOnlineReceivers().get(0));
 
 
     }
 
     public void addContact(String title){
-        contactsList.add(new JCheckBox(title));
+        JCheckBox newBox = new JCheckBox(title);
+        newBox.setName(title);
+        contactsListPanel.add(newBox);
     }
 
     public void addOnline(String title){
         JCheckBox newBox = new JCheckBox(title);
         newBox.setName(title);
-        onlineList.add(newBox);
+        newBox.setSelected(true);
+        onlineListPanel.add(newBox);
     }
 
     public void removeOnline(String title){
-        for(JCheckBox checkbox: onlineList){
-            if(checkbox.getName().equals(title)){
-                onlineList.remove(checkbox);
-                System.out.println("fitta");
+        for(Component c: onlineListPanel.getComponents()){
+            if(c.getName() == title){
+                onlineListPanel.remove(c);
             }
         }
     }
 
-    public void paintLists(){
-        onlineListPanel.removeAll();
-        onlineListPanel.revalidate();
-        onlineListPanel.repaint();
-        onlineListPanel.add(new JLabel("Online:"));
-        for(JCheckBox checkbox: onlineList){
-            onlineListPanel.add(checkbox);
-        }
 
-        contactsListPanel.removeAll();
-        contactsListPanel.add(new JLabel("Contacts:"));
-        for(JCheckBox checkbox: contactsList){
-            contactsListPanel.add(checkbox);
+    public ArrayList<String> getListofOnlineReceivers(){
+        ArrayList<String> receivers = new ArrayList<>();
+        JCheckBox[] boxes = (JCheckBox[]) onlineListPanel.getComponents();
+        for(JCheckBox c: boxes){
+            if(c.isSelected()){
+                receivers.add(c.getName());
+            }
         }
+        return receivers;
     }
 
+    public ArrayList<String> getListofContactReceivers(){
+        ArrayList<String> receivers = new ArrayList<>();
+        return null;
+    }
 
 }
